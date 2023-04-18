fc4dc3bcdc7a6997ac4161463d7d5b8b6ef6c833
KyotoKhan <70768038+KyotoKhan@users.noreply.github.com>
2023-04-18 18:19:47 -0500

    Add files via upload
    New admin functionality added to the Userlogin.java

---
+++ CopyOfNewAdminFunction/UserAccount.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.useraccount;
+
+/**
+ *
+ * @author Christian Tran
+ */
+public class UserAccount {
+
+    public static void main(String[] args) {
+       
+    }
+}
